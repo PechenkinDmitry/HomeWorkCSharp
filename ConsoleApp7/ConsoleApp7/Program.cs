2b0df40c483c47abefc726f5eec97f3a1c8ea91e
DmitryPechenkin <dimape4enb1998@gmail.com>
2023-04-16 15:42:53 +0300

    HOMEWORK4444

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        switch (a)
+        {
+            case 1:
+                Console.WriteLine("Запустить диагностику");
+                break;
+
+            case 2:
+                Console.WriteLine("Вылечить поврежденные блоки системы");
+                break;
+
+            case 'e':
+                Console.WriteLine("Перезапустить пункт управления");
+                break;
+
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO.Compression;
+using System.Runtime;
+
+if (!Directory.Exists("C:\\Lessons\\ConsoleApp5\\ZipInfo"))
+{
+    Directory.CreateDirectory("C:\\Lessons\\ConsoleApp5\\ZipInfo");
+}
+ZipFile.ExtractToDirectory("C:\\Lessons\\ConsoleApp5\\WorkPAPKA.zip", "C:\\Lessons\\ConsoleApp5\\ZipInfo");
+
+if (!File.Exists("C:\\Lessons\\ConsoleApp5\\NewInfo.csv"))
+{
+    File.Create("C:\\Lessons\\ConsoleApp5\\NewInfo.csv");
+}
+var newInfo = "C:\\Lessons\\ConsoleApp5\\NewInfo.csv";
+
+using FileStream fileCSV = new FileStream(newInfo, FileMode.OpenOrCreate);
+using var streamWriter = new StreamWriter(fileCSV);
+DirectoryInfo di = new DirectoryInfo("C:\\Lessons\\ConsoleApp5\\ZipInfo");
+DirectoryInfo[] diArr = di.GetDirectories();
+for (var i = 0; i < diArr.Length;i++ )
+{
+  
+    streamWriter.WriteLine("Directory");
+    streamWriter.WriteLine(diArr[i].Name);
+    streamWriter.WriteLine(diArr[i].LastWriteTime);
+    var del = "C:\\Lessons\\ConsoleApp5\\ZipInfo" + diArr[i].Name;
+    Directory.Delete(del);
+
+}
+Directory.Delete("C:\\Lessons\\ConsoleApp5\\ZipInfo");
+var fileTxt = "C:\\Lessons\\AppData\\Roaming\\Lesson12Homework.txt";
+using var fileStreamSecond = new FileStream(fileTxt, FileMode.OpenOrCreate);
+using var newStreamWriter =  new StreamWriter(fileStreamSecond);
+newStreamWriter.WriteLine(Path.GetFullPath("C:\\Lessons\\ConsoleApp5\\NewInfo.csv"));
